e8f8eb967ab036197a42294536cf35c82e0d1008
413831 <alepleins3pp@gmail.com>
2022-11-26 08:08:18 -0300

    Fix Procfile

---
+++ src/components/Center.js
@@ -0,0 +1,19 @@
+export default function Center(props){
+    console.log(props)
+
+    const { centers } = props;
+
+    return (
+    <div >
+        <p>{JSON.stringify(props)}</p>
+        {centers.map((center,index)=>(
+            <div>
+                <hr></hr>
+                <p><b>ID:</b> {center._id}</p>
+                <p><b>Nombre:</b> {center.name}</p>
+                <p><b>Cantidad de empleados:</b> {center.staff_counter}</p>
+            </div>
+        ))}
+    </div>
+    )
+}
